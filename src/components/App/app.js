6577f41df86b7d34f8fc470e4d04e3982d356f95
Piyush Abbi <piyushabbi27@gmail.com>
2018-01-29 23:20:41 +0530

    Sort title

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import _ from 'lodash';
 
 import Card from '../Card/card';
 
@@ -8,45 +9,79 @@ class App extends Component {
     super(props);
     this.state = {
       data: [],
-      searchText: ""
+      searchText: "",
+      sortByValue: "endDate"
     };
   }
+
+  requestHandler = () => {
+    axios.get("http://localhost:4000/data").then(res => {
+      let data = res.data.filter(f => {
+        return f.title
+            .toLowerCase()
+            .indexOf(this.state.searchText.toLowerCase()) > -1;
+      });
+      this.setState({ data });
+    });
+  }
+  
   inputChangeHandler = e => {
     this.setState({ searchText: e.target.value });
-    axios.get("http://localhost:4000/data")
-      .then(res => {
-        let data = res.data.filter(f => {
-          return f.title.toLowerCase().indexOf(this.state.searchText.toLowerCase()) > -1;
+    
+    let debounced = _.debounce(this.requestHandler, 250, { maxWait: 1000 });
+    //this.requestHandler();
+    debounced()
+  }
+  sortHandler = (value) => {
+    switch (value) {
+      case 'title': {
+        const data = this.state.data;
+        let sortedData = data.sort((a, b) => a.title > b.title);
+        this.setState(prevState => {
+          return [...prevState.data, ...sortedData];
         });
-        this.setState({ data });
-      });
+        break;
+      }
+      default: break;
+    }
+  }
+  selectChangeHandler = (e) => {
+    this.setState({
+      sortByValue: e.target.value
+    });
+    setTimeout(() => {
+      this.sortHandler(this.state.sortByValue);
+    })    
   }
   
   render() {
-    return (
-      <div className="container">
+    return <div className="container">
         <h1>Products</h1>
-        {/* Start: Input Search Component */}
-        <input
-          type="search"
-          value={this.state.searchText}
-          onChange={this.inputChangeHandler}
-          placeholder="Search for products"
-        />
-        {/* End: Input Search Component */}
+        <div className="row">
+          <div className="col s8">
+            {/* Start: Input Search Component */}
+            <input type="search" value={this.state.searchText} onChange={this.inputChangeHandler} placeholder="Search for products" />
+            {/* End: Input Search Component */}
+          </div>
+          <div className="col s4">
+            <label>Sort By:-</label>
+            <select style={{display: 'block'}} value={this.state.sortByValue} onChange={this.selectChangeHandler}>
+              <option value="title">Title</option>
+              <option value="percentageFunded">Percentage Funded</option>
+              <option value="endDate">End Date</option>
+            </select>
+          </div>
+        </div>
         <div className="row">
           {/* Start: Search Results */}
-          {
-            this.state.searchText === ""
-            ? <p className="center">Enter search text.</p>
-            : this.state.data.length > 0
-              ? this.state.data.map(m => <Card key={m["s.no"]} data={m} /> )
-              : <p className="center">No Match Found!</p>
-          }
+          {this.state.searchText === "" ? <p className="">
+              Enter search text.
+            </p> : this.state.data.length > 0 ? this.state.data.map(m => (
+              <Card key={m["s.no"]} data={m} />
+            )) : <p className="">No Match Found!</p>}
           {/* End: Search Results */}
         </div>
-      </div>
-    );
+      </div>;
   }
 }
 
